310ca8fe9c8585cf11c07aaa79cc7de7e5d1b8ed
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-02-20 12:02:17 -0300

    Request 01 OK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
-class App extends React.Component {
-  render() {
-    return (
-      <p>Sistema Solar</p>
-    );
-  }
+function App() {
+  return (
+    <Header />
+  );
 }
 
 export default App;
